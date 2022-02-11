a29f8139a1f708ce8d9a8f1b081091f3af27ac39
ivv11@alu.ua.es <ivv11@alu.ua.es>
2022-02-11 15:07:52 +0100

    Cambios iniciales en la Práctica 1

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -1,9 +1,11 @@
 // Programación 2 - Práctica 1
 // DNI: 20525805F
-// Nombre: IVAN VALOR VERDU
+// Nombre: IVAN VALOR VERDú
 
 #include <iostream>
 #include <cstdlib> // Para rand(), srand() y atoi()
+#include <cctype>
+#include<cstring>
 
 using namespace std;
 
@@ -12,13 +14,13 @@ const int KENEMIES=5;
 const int KPOINTS=200;
 const int KDICE=20; // Número de caras del dado
 
-struct Core{
+struct Core{ //centro
   int attack;
   int defense;
   int hp;
 };
 
-enum Breed{
+enum Breed{ //raza
   AXOLOTL,
   TROLL,
   ORC,
@@ -26,7 +28,7 @@ enum Breed{
   DRAGON
 };
 
-struct Enemy{
+struct Enemy{ 
   Breed name;
   Core features;
 };
@@ -45,9 +47,44 @@ int rollDice(){
 }
 
 Hero createHero(){
+	Hero leerHero;
+	bool es; //comprobar que el nombre es todo numeros o caracteres
+	es=true; //es true y si cambia se repite el bucle
+	do{
+		cout << "Enter hero name: ";
+		cin.getline(leerHero.name, KNAME);
+		if(isalpha(leerHero.name[0])==false){
+			cout << "ERROR: wrong name" << endl;
+		}
+		else{
+			for(int i=0; (unsigned int)i < strlen(leerHero.name); i++){
+				if(isalnum(leerHero.name[i])== 0){
+					es=false;
+				}
+			}
+			if(es==false){
+				cout << "ERROR: wrong name" << endl;
+			}
+		}
+	}while(isalpha(leerHero.name[0])==false || es == false);
+	
+	do{
+		cout << "Enter attack/defense: ";
+		cin >> leerHero.features.attack;
+		cin.get();
+		cin >> leerHero.features.defense;
+		if(leerHero.features.attack + leerHero.features.defense != 100 && leerHero.features.attack < 0 && leerHero.features.defense < 0){
+			cout << "ERROR: wrong distribution" << endl;
+		}
+	}while(leerHero.features.attack + leerHero.features.defense != 100 && leerHero.features.attack < 0 && leerHero.features.defense < 0);
+
+	return leerHero;
 }
 
 Enemy createEnemy(){
+	Enemy leerEnemy;
+		
+	return leerEnemy;
 }
 
 void fight(Hero &hero,Enemy &enemy){
@@ -67,6 +104,7 @@ void showMenu(){
 }
 
 int main(int argc,char *argv[]){
+	Hero myHero;
   if(argc!=2){ // Si los parámetros no son correctos, el programa termina inmediatamente
     cout << "Usage: " << argv[0] << " <seed>" << endl;
   }
@@ -74,5 +112,6 @@ int main(int argc,char *argv[]){
     srand(atoi(argv[1])); // Introducimos la semilla para generar números aleatorios
     
     // Aquí vendrá todo tu código del "main"...
+	myHero=createHero();
   }
 }
